b3955d9f840006bc2cbcb53e48fb836e0c4355e7
RodriMPC <rodriperezcolman@gmail.com>
2024-03-21 22:42:24 -0300

    Primer commit

---
+++ PérezColman-1eraEntrega/src/app.js
@@ -0,0 +1,27 @@
+
+
+/** PRIMER ENTREGA DEL TP FINAL  **/
+
+const express = require("express");
+const app = express(); 
+const PUERTO = 8080;
+const productsRouter = require("./routes/products.router.js");
+const cartsRouter = require("./routes/carts.router.js");
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.get("/", (req, res) => {
+    res.send("Vamos con la Primera Entrega");
+})
+
+
+app.use("/api", productsRouter);
+app.use("/api", cartsRouter);
+
+
+
+app.listen(PUERTO, () => {
+    console.log(`Escuchando en el puerto: ${PUERTO}`);
+})
